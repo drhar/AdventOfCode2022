6a32fe3ae62e272e35c0d2ba266c875e883b2cb8
Dan Harcombe <danharcombe@microsoft.com>
2024-07-25 18:01:25 +0100

    Day 1-3

--- src/day01.rs
+++ src/day01.rs
@@ -1,26 +1,74 @@
 pub fn day01(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
-    let answer2 = 0;
+    let elf_lines = input_lines.split("\n\n");
+    let mut elves: Vec<i32> = vec![];
+    for elf in elf_lines {
+        elves.push(elf.lines().map(parse_elf).sum());
+    }
+    elves.sort();
+
+    let answer1 = elves.last().unwrap();
+    let answer2 = elves[elves.len() - 3] + elves[elves.len() - 2] + elves[elves.len() - 1];
     (format!("{}", answer1), format!("{}", answer2))
 }
 
+pub fn parse_elf(elf: &str) -> i32 {
+    elf.parse::<i32>().expect(&format!("Expected number for {}", elf))
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn check_day01_part1_case1() {
-        assert_eq!(day01("").0, "0".to_string())
+        assert_eq!(day01("1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000").0, "24000".to_string())
     }
 
     #[test]
     fn check_day01_part2_case1() {
-        assert_eq!(day01("").1, "0".to_string())
+        assert_eq!(day01("1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000").1, "45000".to_string())
     }
 
     #[test]
     fn check_day01_both_case1() {
-        assert_eq!(day01(""), ("0".to_string(), "0".to_string()))
+        assert_eq!(day01("1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"), ("24000".to_string(), "45000".to_string()))
     }
 }
--- src/day02.rs
+++ src/day02.rs
@@ -1,8 +1,87 @@
 pub fn day02(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
-    let answer2 = 0;
+    let mut answer1 = 0;
+    let mut answer2 = 0;
+    input_lines.lines().map(|line| {
+        let mut iter = line.split_whitespace();
+        let opponent = iter.next().unwrap();
+        let player = iter.next().unwrap();
+        match player {
+            "X" => {
+                answer1 += 1 + rock_fight(opponent);
+                answer2 += 0 + lose_fight(opponent);
+            },
+            "Y" => {
+                answer1 += 2 + paper_fight(opponent);
+                answer2 += 3 + draw_fight(opponent);
+            },
+            "Z" => {
+                answer1 += 3 + scissor_fight(opponent);
+                answer2 += 6 + win_fight(opponent);
+            },
+            _ => panic!("Invalid move by player! {player}")
+        }
+    }).for_each(drop);
     (format!("{}", answer1), format!("{}", answer2))
+}
+
+pub fn rock_fight(opp: &str) -> i32 {
+    let result_score = match opp {
+        "A" => 3,
+        "B" => 0,
+        "C" => 6,
+        _ => panic!("Invalid move by opponent! {opp}")
+    };
+    result_score
+}
+
+pub fn paper_fight(opp: &str) -> i32 {
+    let result_score = match opp {
+        "A" => 6,
+        "B" => 3,
+        "C" => 0,
+        _ => panic!("Invalid move by opponent! {opp}")
+    };
+    result_score
+}
+
+pub fn scissor_fight(opp: &str) -> i32 {
+    let result_score = match opp {
+        "A" => 0,
+        "B" => 6,
+        "C" => 3,
+        _ => panic!("Invalid move by opponent! {opp}")
+    };
+    result_score
+}
+
+pub fn lose_fight(opp: &str) -> i32 {
+    let result_score = match opp {
+        "A" => 3,
+        "B" => 1,
+        "C" => 2,
+        _ => panic!("Invalid move by opponent! {opp}")
+    };
+    result_score
+}
+
+pub fn draw_fight(opp: &str) -> i32 {
+    let result_score = match opp {
+        "A" => 1,
+        "B" => 2,
+        "C" => 3,
+        _ => panic!("Invalid move by opponent! {opp}")
+    };
+    result_score
+}
+
+pub fn win_fight(opp: &str) -> i32 {
+    let result_score = match opp {
+        "A" => 2,
+        "B" => 3,
+        "C" => 1,
+        _ => panic!("Invalid move by opponent! {opp}")
+    };
+    result_score
 }
 
 #[cfg(test)]
@@ -11,16 +90,16 @@ mod tests {
 
     #[test]
     fn check_day02_part1_case1() {
-        assert_eq!(day02("").0, "0".to_string())
+        assert_eq!(day02("A Y").0, "8".to_string())
     }
 
     #[test]
     fn check_day02_part2_case1() {
-        assert_eq!(day02("").1, "0".to_string())
+        assert_eq!(day02("B X").1, "1".to_string())
     }
 
     #[test]
     fn check_day02_both_case1() {
-        assert_eq!(day02(""), ("0".to_string(), "0".to_string()))
+        assert_eq!(day02("C Z"), ("6".to_string(), "7".to_string()))
     }
 }
