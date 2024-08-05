8aaa4854ee4640e3e9d81916639a3bdc7878a6e9
Dan Harcombe <danharcombe@microsoft.com>
2024-08-05 16:39:52 +0100

    Now that we're reverse iterating from trees as part of figuring out their views, it's quicker and easier to update bottom right visible as part of the single woodland walk, so refactor to do that and remove all the complexity

--- src/day08.rs
+++ src/day08.rs
@@ -3,13 +3,7 @@ use std::collections::HashSet;
 const MAX_TREE_HEIGHT: i32 = 9;
 
 pub fn day08(input_lines: &str) -> (String, String) {
-    let bottom_right = input_lines.chars().rev().collect::<String>();
-    let (tl_visible, trees) = top_left_woodland_walk(input_lines, true);
-    println!("{:?}", tl_visible);
-    let (br_visible, _) = top_left_woodland_walk(&bottom_right, false);
-    let visible = tl_visible
-        .union(&br_visible)
-        .collect::<Vec<&(usize, usize)>>();
+    let (visible, trees) = woodland_walk(input_lines);
     let answer1 = visible.len();
 
     // Debug prints
@@ -43,10 +37,7 @@ struct Tree {
     down_view: usize,
 }
 
-fn top_left_woodland_walk(
-    woodland: &str,
-    top_left_is_nw: bool,
-) -> (HashSet<(usize, usize)>, Vec<Vec<Tree>>) {
+fn woodland_walk(woodland: &str) -> (HashSet<(usize, usize)>, Vec<Vec<Tree>>) {
     let mut woodland = woodland.lines().peekable();
     let column_count = woodland.peek().unwrap().len();
     let row_count: usize = woodland.clone().count();
@@ -63,47 +54,39 @@ fn top_left_woodland_walk(
         ];
         row_count
     ];
-    let mut top_maxes: Vec<i32> = vec![-1; column_count];
-    let mut bottom_maxes: Vec<i32> = vec![-1; column_count];
-    let mut top_visible: Vec<HashSet<(usize, usize)>> = vec![HashSet::new(); column_count];
+    let mut column_maxes: Vec<i32> = vec![-1; column_count];
+    let mut column_visible: Vec<HashSet<(usize, usize)>> = vec![HashSet::new(); column_count];
 
     let mut outside_visible =
         HashSet::from_iter(woodland.enumerate().flat_map(|(row_idx, row)| {
             let mut left_max: i32 = -1;
-            let mut right_max: i32 = -1;
-            let row_left_visible = row
+            let row_visible = row
                 .chars()
                 .enumerate()
-                .filter_map(|(col_idx, cell)| {
-                    let (x_cd, y_cd) = match top_left_is_nw {
-                        true => (col_idx, row_idx),
-                        false => (column_count - 1 - col_idx, row_count - 1 - row_idx),
-                    };
-                    let tree_coords = (x_cd, y_cd);
-                    let mut visible = None;
+                .map(|(col_idx, cell)| {
+                    let (x, y) = (col_idx, row_idx);
+                    let mut visible: HashSet<(usize, usize)> = HashSet::new();
                     let height = cell.to_string().parse::<i32>().unwrap();
                     let mut left_view = 0;
                     let mut up_view = 0;
 
                     // If our woodland is more than a single tree and we're not on the left/top, there must be at least one tree.
-                    let x = col_idx;
-                    let y = row_idx;
                     if x > 0 {
                         left_view += 1;
                     }
                     if y > 0 {
                         up_view += 1;
                     }
-                    // Work out left view of tree
+                    // Work out view of this tree to the left.
                     while left_view < x {
                         let left_tree = &trees[y][x - left_view];
                         if left_tree.height >= height {
                             break;
                         }
-                        // We can definitely see over any tree this tree can see over
+                        // We can definitely see over any tree this tree can see over.
                         left_view += left_tree.left_view;
                     }
-                    // Update right view of previous trees. No trees left of our left_view could see us.
+                    // Update view right from previous trees. No trees left of our left_view could see us.
                     let mut left_tree_idx = x - left_view;
                     while left_tree_idx < x {
                         let left_tree = &mut trees[y][left_tree_idx];
@@ -120,24 +103,8 @@ fn top_left_woodland_walk(
                             left_tree_idx += left_tree.right_view;
                         }
                     }
-                    // Right edge clean-up, any trees taller than us and to our left can see the whole way.
-                    // No need to go further than heighest tree.
-                    if x == column_count - 1 {
-                        let mut left_tree_idx = x - left_view;
-                        let mut left_tree = &mut trees[y][left_tree_idx];
-                        let mut left_highest = left_tree.height;
-                        left_tree.right_view = left_view;
-                        while left_highest < MAX_TREE_HEIGHT && left_tree_idx > 0 {
-                            if left_tree.height > left_highest {
-                                left_highest = left_tree.height;
-                                left_tree.right_view = x - left_tree_idx;
-                            }
-                            left_tree_idx -= left_tree.left_view;
-                            left_tree = &mut trees[y][left_tree_idx];
-                        }
-                    }
 
-                    // Work out view down from this tree. Same process as left view but change y coordinate.
+                    // Work out view ups from this tree. Same process as left view but change y coordinate.
                     while up_view < y {
                         let top_tree = &trees[y - up_view][x];
                         if top_tree.height >= height {
@@ -147,7 +114,7 @@ fn top_left_woodland_walk(
                         up_view += top_tree.up_view;
                     }
 
-                    // Update down view of previous trees. Same process as right view but change y coordinate.
+                    // Update view down from previous trees. Same process as right view but change y coordinate.
                     let mut top_tree_idx = y - up_view;
                     while top_tree_idx < y {
                         let top_tree = &mut trees[top_tree_idx][x];
@@ -164,23 +131,6 @@ fn top_left_woodland_walk(
                         }
                     }
 
-                    // Bottom edge clean-up, any trees taller than us and above us can see the whole way.
-                    // No need to go further than heighest tree.
-                    if y == row_count - 1 {
-                        let mut up_tree_idx = y - up_view;
-                        let mut up_tree = &mut trees[up_tree_idx][x];
-                        let mut up_highest = up_tree.height;
-                        up_tree.down_view = up_view;
-                        while up_highest < MAX_TREE_HEIGHT && up_tree_idx > 0 {
-                            if up_tree.height > up_highest {
-                                up_highest = up_tree.height;
-                                up_tree.down_view = y - up_tree_idx;
-                            }
-                            up_tree_idx -= up_tree.up_view;
-                            up_tree = &mut trees[up_tree_idx][x];
-                        }
-                    }
-
                     let curr_tree = &mut trees[y][x];
                     curr_tree.height = height;
                     curr_tree.left_view = left_view;
@@ -189,21 +139,59 @@ fn top_left_woodland_walk(
                     curr_tree.down_view = 0;
 
                     // Decide if this tree is visible from the top
-                    if height > top_maxes[col_idx] {
-                        top_maxes[col_idx] = height;
-                        top_visible[col_idx].insert(tree_coords);
+                    if height > column_maxes[x] {
+                        column_maxes[x] = height;
+                        column_visible[x].insert((x, y));
                     }
                     // Decide if this tree is visible from the left
                     if height > left_max {
                         left_max = height;
-                        visible = Some(tree_coords)
+                        visible.insert((x, y));
                     }
+
+                    // Right edge. Clean-up right views of trees that can see out of the woodland. These are also the trees that
+                    // are visible from the outside on the right. No need to go further than heighest tree.
+                    if x == column_count - 1 {
+                        // Outer tree always visible
+                        visible.insert((x, y));
+                        let mut left_tree_idx = x;
+                        let mut left_highest = height;
+                        while left_highest < left_max && left_tree_idx > 0 {
+                            let left_tree = &mut trees[y][left_tree_idx];
+                            if left_tree.height > left_highest {
+                                left_highest = left_tree.height;
+                                left_tree.right_view = x - left_tree_idx;
+                                visible.insert((left_tree_idx, y));
+                            }
+                            left_tree_idx -= left_tree.left_view;
+                        }
+                    }
+
+                    // Bottom edge, set outside visibility and downwards visibility as with right edge.
+                    if y == row_count - 1 {
+                        column_visible[col_idx].insert((x, y));
+                        let mut up_tree_idx = y;
+                        let mut up_highest = height;
+                        while up_highest < column_maxes[x] && up_tree_idx > 0 {
+                            let up_tree = &mut trees[up_tree_idx][x];
+                            if up_tree.height > up_highest {
+                                up_highest = up_tree.height;
+                                up_tree.down_view = y - up_tree_idx;
+                                column_visible[col_idx].insert((x, y));
+                            }
+                            up_tree_idx -= up_tree.up_view;
+                        }
+                    }
+
                     visible
                 })
-                .collect::<Vec<(usize, usize)>>();
-            row_left_visible.into_iter()
+                .filter(|visible| !visible.is_empty())
+                .flatten()
+                // We force this so that we can use the nested enumeration
+                .collect::<HashSet<(usize, usize)>>();
+            row_visible.into_iter()
         }));
-    for column in top_visible {
+    for column in column_visible {
         outside_visible.extend(column);
     }
     (outside_visible, trees)
