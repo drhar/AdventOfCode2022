ec8e109acc122d7ed8042b8fb6013a6feaff3542
Dan Harcombe <danharcombe@microsoft.com>
2024-08-08 10:44:58 +0100

    Refactor into a Map object to make the design nicer - most of the functions were doing things to squares, not a property of squares themselves. Design is now someone drawing on a map made of squares

--- src/day12.rs
+++ src/day12.rs
@@ -1,109 +1,76 @@
-// This could do with a step back and re-design both from OOP perspective & "Surely this sort of thing is a
-// solved problem and the algorithm I just came up with won't be the best one" perspective :).
+// Surely this sort of thing is a solved problem and the algorithm I just came up with won't be the best one. Consider looking up
 use std::collections::HashSet;
 
 pub fn day12(input_lines: &str) -> (String, String) {
-    let mut start = (0, 0);
-    let mut end = (0, 0);
-    let max_x = input_lines.lines().next().unwrap().len() - 1;
-    let max_y = input_lines.lines().count() - 1;
-    let mut map = input_lines
-        .lines()
-        .enumerate()
-        .map(|(y, line)| {
-            line.chars()
-                .enumerate()
-                .map(|(x, height)| {
-                    let height = match height {
-                        'a'..='z' => height as i32 - 'a' as i32,
-                        'S' => {
-                            start = (x, y);
-                            'a' as i32 - 'a' as i32
-                        }
-                        'E' => {
-                            end = (x, y);
-                            'z' as i32 - 'a' as i32
-                        }
-                        h => panic!("Unexpected height: {}", h),
-                    };
-                    Square::new(height, (x, y), max_x, max_y)
-                })
-                .collect::<Vec<Square>>()
-        })
-        .collect::<Vec<Vec<Square>>>();
-    for x in 0..=max_x {
-        for y in 0..=max_y {
-            let neighbours = calculate_access((x, y), &map);
-            map[y][x].accessible_from = Some(neighbours);
-        }
-    }
-    walk_route_backwards(HashSet::from([end]), &mut map, 0);
+    let mut map = Map::from_str(input_lines);
+    map.plot_accessible_borders();
+    map.plot_routes_backwards(HashSet::from([map.end]), 0);
 
-    let answer1 = map[start.1][start.0].steps_from_end.unwrap();
+    let answer1 = map.grid[map.start.1][map.start.0].steps_from_end.unwrap();
     let answer2 = map
+        .grid
         .iter()
-        .map(|row| row.iter())
-        .flatten()
-        .filter(|square| square.height == 0 && square.steps_from_end.is_some())
-        .map(|square| square.steps_from_end.unwrap())
+        .flat_map(|row| row.iter())
+        .filter_map(|square| {
+            if square.steps_from_end.is_none() || square.height != 0 {
+                None
+            } else {
+                square.steps_from_end
+            }
+        })
         .min()
         .unwrap();
     (format!("{}", answer1), format!("{}", answer2))
 }
 
-pub fn walk_route_backwards(
-    neighbours: HashSet<(usize, usize)>,
-    map: &mut Vec<Vec<Square>>,
-    starting_steps: i32,
-) {
-    let visited = neighbours.clone();
-    for neighbour in neighbours {
-        let new_neighbours = map[neighbour.1][neighbour.0].extend_path(starting_steps);
-
-        // New neighbours will always be at least one extra step away, so don't bother visiting nodes we're going to visit at this
-        // level from within them.
-        let new_neighbours = match new_neighbours {
-            Ok(neighbours) => neighbours
-                .difference(&visited)
-                .cloned()
-                .collect::<HashSet<(usize, usize)>>(),
-            Err(_) => continue,
-        };
-        walk_route_backwards(new_neighbours, map, starting_steps + 1);
-    }
+pub struct Map {
+    grid: Vec<Vec<Square>>,
+    max_x: usize,
+    max_y: usize,
+    start: (usize, usize),
+    end: (usize, usize),
 }
 
-pub fn calculate_access(square: (usize, usize), map: &Vec<Vec<Square>>) -> HashSet<(usize, usize)> {
-    let square = &map[square.1][square.0];
-    let mut accessible_neighbours = HashSet::new();
-    for &(x, y) in &square.neighbours {
-        let neighbour_square = &map[y][x];
-        if square.height <= neighbour_square.height + 1 {
-            accessible_neighbours.insert((x, y));
-        }
-    }
-    accessible_neighbours
-}
-
-#[derive(Debug)]
-pub struct Square {
-    height: i32,
-    neighbours: Vec<(usize, usize)>,
-    accessible_from: Option<HashSet<(usize, usize)>>,
-    steps_from_end: Option<i32>,
-}
-
-impl Square {
-    pub fn new(height: i32, position: (usize, usize), max_x: usize, max_y: usize) -> Self {
-        Self {
-            height,
-            neighbours: Self::calculate_neighbours(position, max_x, max_y),
-            accessible_from: None,
-            steps_from_end: None,
+impl Map {
+    fn from_str(input_lines: &str) -> Self {
+        let mut start = (0, 0);
+        let mut end = (0, 0);
+        let max_x = input_lines.lines().next().unwrap().len() - 1;
+        let max_y = input_lines.lines().count() - 1;
+        let grid = input_lines
+            .lines()
+            .enumerate()
+            .map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(x, height)| {
+                        let height = match height {
+                            'a'..='z' => height as i32 - 'a' as i32,
+                            'S' => {
+                                start = (x, y);
+                                0
+                            }
+                            'E' => {
+                                end = (x, y);
+                                'z' as i32 - 'a' as i32
+                            }
+                            h => panic!("Unexpected height: {}", h),
+                        };
+                        Square::new(height, Map::cell_neighbours((x, y), max_x, max_y))
+                    })
+                    .collect::<Vec<Square>>()
+            })
+            .collect::<Vec<Vec<Square>>>();
+        Map {
+            grid,
+            max_x,
+            max_y,
+            start,
+            end,
         }
     }
 
-    fn calculate_neighbours(
+    fn cell_neighbours(
         position: (usize, usize),
         max_x: usize,
         max_y: usize,
@@ -125,13 +92,82 @@ impl Square {
         neighbours
     }
 
-    pub fn extend_path(&mut self, steps: i32) -> Result<HashSet<(usize, usize)>, &str> {
-        if self.steps_from_end.is_none() || self.steps_from_end.unwrap() > steps {
-            self.steps_from_end = Some(steps);
-            return Ok(self.accessible_from.clone().unwrap());
+    // Go through every square on a populated map and mark which squares are accessible from where.
+    // (Like drawing little connecting lines on a paper map).
+    pub fn plot_accessible_borders(&mut self) {
+        for x in 0..=self.max_x {
+            for y in 0..=self.max_y {
+                let mut accessible_from = HashSet::new();
+                let square = &self.grid[y][x];
+                for &(i, j) in &square.neighbours {
+                    if square.height <= self.grid[j][i].height + 1 {
+                        accessible_from.insert((i, j));
+                    }
+                }
+                self.grid[y][x].accessible_from = Some(accessible_from);
+            }
+        }
+    }
+
+    // Visit a square, if we've not been there before or we're here faster than previously, update the square and
+    // return the next next squares we could visit.
+    // (Imagine keeping a little paper mark on each square of your map and updating it each time you try a new route)
+    pub fn visit_square(
+        &mut self,
+        square: (usize, usize),
+        steps: i32,
+    ) -> Result<HashSet<(usize, usize)>, &str> {
+        let square = &mut self.grid[square.1][square.0];
+        if square.steps_from_end.is_none() || square.steps_from_end.unwrap() > steps {
+            square.steps_from_end = Some(steps);
+            return Ok(square.accessible_from.clone().unwrap());
         }
 
         Err("Already visited in a better way")
+    }
+
+    // For a possible set of coordinate squares, walk backwards through all possible routes until we've gone as far as we can for each
+    // without revisiting a square.
+    // (This one is just getting the red pencil out and drawing routes, giving up when it's clear a previous route was better!)
+    pub fn plot_routes_backwards(
+        &mut self,
+        route_options: HashSet<(usize, usize)>,
+        starting_steps: i32,
+    ) {
+        let visited = route_options.clone();
+        for square in route_options {
+            let next_steps = self.visit_square(square, starting_steps);
+
+            // New neighbours will always be at least one extra step away, so don't bother visiting nodes we're going to visit at this
+            // level from within them.
+            let next_steps = match next_steps {
+                Ok(steps) => steps
+                    .difference(&visited)
+                    .cloned()
+                    .collect::<HashSet<(usize, usize)>>(),
+                Err(_) => continue,
+            };
+            self.plot_routes_backwards(next_steps, starting_steps + 1);
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Square {
+    height: i32,
+    neighbours: Vec<(usize, usize)>,
+    accessible_from: Option<HashSet<(usize, usize)>>,
+    steps_from_end: Option<i32>,
+}
+
+impl Square {
+    pub fn new(height: i32, neighbours: Vec<(usize, usize)>) -> Self {
+        Self {
+            height,
+            neighbours,
+            accessible_from: None,
+            steps_from_end: None,
+        }
     }
 }
 
